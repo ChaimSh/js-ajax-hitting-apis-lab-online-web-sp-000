bea4331556b9f781542e8eb3c0b9cdd9b724bafe
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-15 00:10:45 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // your code here
+const rootURL = 'https://api.github.com';
+
+function getRepositories(){
+  const name  = document.getElementById('username').value;
+
+  const uri = rootURL + '/users/' + name + '/repos'
+
+  const req = new XMLHttpRequest();
+
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', uri);
+  req.send();
+  return false;
+
+}
+
+function displayRepositories(){
+  const repos = JSON.parse(this.responseText);
+
+  const repoList = `<ul>` + repos.map(repo => {
+  const dataUsername = 'data-username="' + repo.owner.login + '"';
+  const dataRepoName = 'data-repository="' + repo.name + '"';
+  return `<li>
+  <h2>${repo.name}</h2>
+  <a href="${repo.html_url}">${repo.html_url}</a><br>
+  <a href = "#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+  <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+  </li>`;
+}).join('') + '</ul>';
+document.getElementById('repositories').innerHTML = repoList;
+}
+
+function
